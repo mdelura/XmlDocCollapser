1b28c72a06f1112c937b41a4932e20ff3e9c8f0a
Michal Delura <deluramichal@gmail.com>
2018-03-20 23:25:28 +0100

    Add TextViewCreationListener

---
+++ XmlDocCollapser/TextViewCreationListener.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.Utilities;
+
+namespace XmlDocCollapser
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    class TextViewCreationListener : IWpfTextViewCreationListener
+    {
+        [Import]
+        internal IOutliningManagerService OutliningManagerService { get; set; }
+        
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            IOutliningManager outliningManager = OutliningManagerService.GetOutliningManager(textView);
+
+            outliningManager.RegionsChanged += OutliningManager_RegionsChanged;
+        }
+
+        private void OutliningManager_RegionsChanged(object sender, RegionsChangedEventArgs e)
+        {
+            const string xmlDocCollapsedFormStart = "/// <summary>";
+
+            IOutliningManager outliningManager = sender as IOutliningManager;
+
+            outliningManager.CollapseAll(e.AffectedSpan, c => c.CollapsedForm.ToString().StartsWith(xmlDocCollapsedFormStart) && !c.IsCollapsed && c.IsCollapsible);
+
+            //Unsubscribe from RegionsChanged after initial change
+            outliningManager.RegionsChanged -= OutliningManager_RegionsChanged;
+        }
+    }
+}
